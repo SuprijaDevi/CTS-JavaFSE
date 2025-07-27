c3ff89528f5077e28e742695aee66a222b5db873
SuprijaDevi <ropotther@gmail.com>
2025-07-27 16:50:11 +0530

    Week6

---
+++ Week6 - React/ReactJS- HOL/scorecalculatorapp/src/App.js
@@ -0,0 +1,16 @@
+import CalculateScore from './Component/CalculateScore';
+
+function App() {
+  return (
+    <div className="App">
+      <CalculateScore
+         Name="Steeve"
+        School="DAV Public School"
+        total={284}
+        goal={3}
+      />
+    </div>
+  );
+}
+
+export default App;
